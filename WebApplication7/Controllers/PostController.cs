7d153bb45a157c9ef068ce05df3a5b6978c5ff41
Carlos Pivotto <carlospivotto@gmail.com>
2020-12-03 16:38:01 -0300

    Inicial

---
+++ WebApplication7/Controllers/PostController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: PostController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Post post)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Fazer o upload
+                    post.Image = UploadImage(post.ImageFile).Result;
+                    return Redirect("/");
+                }
+                return View(post);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        private async Task<string> UploadImage(IFormFile imageFile)
+        {
+            var reader = imageFile.OpenReadStream();
+            var cloudStorageAccount = CloudStorageAccount.Parse(@"DefaultEndpointsProtocol=https;AccountName=mystorage202012030014;AccountKey=lAbkrtTBzVQY179CL7zwpKCl7ijW69gPzvMDw6OivuBeQr9iDqF6s4QONXK1FYYAzPou/7olooG+K9dMm9exLQ==;EndpointSuffix=core.windows.net");
+            var blobClient = cloudStorageAccount.CreateCloudBlobClient();
+            var container = blobClient.GetContainerReference("post-images");
+            await container.CreateIfNotExistsAsync();
+            var blob = container.GetBlockBlobReference(Guid.NewGuid().ToString());
+            await blob.UploadFromStreamAsync(reader);
+            var uri = blob.Uri.ToString();
+            return uri;
+        }
+
+        // GET: PostController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication7/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Image { get; set; } // Caminho da imagem já hospedada
+        public IFormFile ImageFile { get; set; } // O buffer do arquivo durante o upload
+    }
+}
